1b03ec61ce9392bf496effd771012fb1077c12ef
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-29 17:16:34 +0330

    first commit

--- rotated_squares.py
+++ rotated_squares.py
@@ -2,15 +2,15 @@ import turtle
 import random
 
 screen = turtle.Screen()
-COLORS = ('red', 'green', 'blue', 'gray', 'black')
+COLORS = ('red', 'green', 'blue', 'gray')
 t = turtle.Pen()
 t.pensize(4)
 for j in range(12):
+    t.pencolor(COLORS[j])
     for i in range(4):
         t.forward(100)
         t.left(90)
     t.left(30)
-    t.pencolor(random.choice(COLORS))
 
 
 screen.exitonclick()
